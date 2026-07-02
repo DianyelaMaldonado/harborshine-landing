a3e8c55a717b4a02261ef132cbcd88d50da52149
Esther Manrique <105511137+EstherManriqueGlez@users.noreply.github.com>
2026-07-02 15:02:24 -0600

    fix(assets): update image paths to relative URLs for consistency

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/harborshine-landing/',
   plugins: [
     tailwindcss({
       // OBLIGAMOS a Tailwind v4 a escanear de forma estricta tus componentes HTML dentro de src
